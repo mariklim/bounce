6405bd7d30d37b9b14404feab89074e912515065
mariklim <maria.klimenko889@gmail.com>
2024-06-08 20:38:33 +0300

    add moon

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $(".celestial").on("mouseover", function(){
+        $('.celestial').effect({
+            effect: 'bounce',
+            direction: 'right',
+            times: 3,
+            distance:10,
+            duration: 3000
+        })
+
+        
+    });
+
+
+ });
