3eda9e1bf6ee4d1073bdae3850efe537df9d1dac
mariklim <maria.klimenko889@gmail.com>
2024-06-09 20:40:21 +0300

    changed blow

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-    $(".celestial").on("mouseover", function(){
-        $('.celestial').effect({
-            effect: 'bounce',
-            direction: 'right',
-            times: 3,
-            distance:10,
-            duration: 3000
-        })
+//     $(".celestial").on("mouseover", function(){
+//         $('.celestial').effect({
+//             effect: 'bounce',
+//             direction: 'right',
+//             times: 6,
+//             distance:10,
+//             duration: 9000 
+//         })
 
         
-    });
+//     });
 
 
- });
+//  });
